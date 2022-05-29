1dd57c9ba425f59156aae3c1d4273a8d7f63123e
kemal9191 <kpehlivan91@gmail.com>
2022-05-29 23:44:14 +0300

    partials created

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const path = require("path");
+const ejs = require("ejs");
+
 
 const app = express();
+app.set("view engine", "ejs")
 
+app.use(express.static("public"))
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog)
+
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+
+  res.render('post');
 });
 
 const port = 3000;
