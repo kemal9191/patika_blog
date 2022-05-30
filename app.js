a34aa970f8b32d6509e3b9bf07360d607e6a3bf3
kemal9191 <kpehlivan91@gmail.com>
2022-05-30 14:59:09 +0300

    post model created

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 const express = require('express');
-const path = require("path");
-const ejs = require("ejs");
-
+const mongoose = require('mongoose');
+const path = require('path');
+const Post = require('./models/Post');
+const ejs = require('ejs');
 
 const app = express();
-app.set("view engine", "ejs")
 
-app.use(express.static("public"))
-app.get('/', (req, res) => {
+mongoose.connect('mongodb://localhost/patika-blog');
 
-  res.render('index');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts: posts,
+  });
 });
 
 app.get('/about', (req, res) => {
-
   res.render('about');
 });
 app.get('/add_post', (req, res) => {
-
   res.render('add_post');
 });
 
 app.get('/post', (req, res) => {
-
   res.render('post');
+});
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
