877f83fe8f1e2549fba972080d86afe4931c24e2
kemal9191 <kpehlivan91@gmail.com>
2022-05-31 22:26:06 +0300

    updated based on mvc architecture

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const Post = require('./models/Post');
 const ejs = require('ejs');
+const { redirect } = require('express/lib/response');
+const methodOverride = require('method-override');
+const postController = require('./controllers/postController')
+const pageControllers = require('./controllers/pageController')
 
 const app = express();
 
@@ -13,33 +16,25 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
-  res.render('index', {
-    posts: posts,
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-
-app.get('/post', (req, res) => {
-  res.render('post');
-});
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
-app.get('/posts/:id', async (req, res)=>{
-  const post = await Post.findById(req.params.id);
-  res.render('post', {
-    post:post
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
   })
-})
+);
+app.get('/', postController.getAllPosts);
+
+app.get('/about', pageControllers.getAboutPage);
+app.get('/add_post', pageControllers.getAddPage);
+
+
+app.post('/posts', postController.createPost);
+app.get('/posts/:id', postController.getPost);
+
+app.delete('/posts/delete/:id', postController.deletePost);
+
+app.put('/posts/:id', postController.updatePost);
+
+app.get('/posts/edit/:id', pageControllers.getEditPage)
 
 const port = 3000;
 
